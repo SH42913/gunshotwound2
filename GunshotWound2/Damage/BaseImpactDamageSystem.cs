6059566d9eca12b9364b7757e5a99ed18c072b39
SH42913 <shoot913@gmail.com>
2021-05-13 23:50:09 +0300

    Decreased impact damage amount

--- GunshotWound2/Damage/BaseImpactDamageSystem.cs
+++ GunshotWound2/Damage/BaseImpactDamageSystem.cs
@@ -10,37 +10,37 @@ namespace GunshotWound2.Damage
     {
         protected void DefaultGrazeWound(int entity)
         {
-            CreateWound(Locale.Data.LightBruise, entity, DamageMultiplier * 5f,
+            CreateWound(Locale.Data.LightBruise, entity, DamageMultiplier * 3f,
                 -1f, PainMultiplier * 15f);
         }
 
         protected void AbrasionWoundOn(string position, int entity)
         {
-            CreateWound($"{Locale.Data.AbrazionWoundOn} {position}", entity, DamageMultiplier * 5f,
+            CreateWound($"{Locale.Data.AbrazionWoundOn} {position}", entity, DamageMultiplier * 3f,
                 BleeedingMultiplier * 0.15f, PainMultiplier * 15f);
         }
 
         protected void LightBruiseWound(string position, int entity)
         {
-            CreateWound($"{Locale.Data.LightBruiseOn} {position}", entity, DamageMultiplier * 5f,
+            CreateWound($"{Locale.Data.LightBruiseOn} {position}", entity, DamageMultiplier * 3f,
                 -1f, PainMultiplier * 15f);
         }
 
         protected void MediumBruiseWound(string position, int entity)
         {
-            CreateWound($"{Locale.Data.MediumBruiseOn} {position}", entity, DamageMultiplier * 10f,
+            CreateWound($"{Locale.Data.MediumBruiseOn} {position}", entity, DamageMultiplier * 8f,
                 -1f, PainMultiplier * 30f, 0.001f);
         }
 
         protected void HeavyBruiseWound(string position, int entity, params CritTypes?[] crits)
         {
-            CreateWound($"{Locale.Data.HeavyBruiseOn} {position}", entity, DamageMultiplier * 15f,
+            CreateWound($"{Locale.Data.HeavyBruiseOn} {position}", entity, DamageMultiplier * 10f,
                 -1f, PainMultiplier * 45f, 0.05f, crits);
         }
 
         protected void WindedFromImpact(int entity)
         {
-            CreateWound(Locale.Data.WindedFromImpact, entity, DamageMultiplier * 15f, -1, PainMultiplier * 40f);
+            CreateWound(Locale.Data.WindedFromImpact, entity, DamageMultiplier * 10f, -1, PainMultiplier * 40f);
         }
     }
 }
