6fbd48c9b5cc3c94b40257a79bfaacd2aae2e889
SH42913 <Shoot913@gmail.com>
2023-07-25 12:12:08 +0400

    SHVDN logger

---
+++ GunshotWound2/Utils/Loggers.cs
@@ -0,0 +1,37 @@
+﻿using SHVDN;
+
+namespace GunshotWound2.Utils {
+    public interface ILogger {
+        void WriteInfo(string message);
+        void WriteWarning(string message);
+        void WriteError(string message);
+    }
+
+    public sealed class ScriptHookLogger : ILogger {
+        private readonly Console consoleInstance;
+        private readonly string[] cachedMessages;
+
+        public ScriptHookLogger() {
+            consoleInstance = System.AppDomain.CurrentDomain.GetData("Console") as Console;
+            cachedMessages = new string[1];
+        }
+
+        public void WriteInfo(string message) {
+            consoleInstance.PrintInfo(message);
+            cachedMessages[0] = message;
+            Log.Message(Log.Level.Info, cachedMessages);
+        }
+
+        public void WriteWarning(string message) {
+            consoleInstance.PrintWarning(message);
+            cachedMessages[0] = message;
+            Log.Message(Log.Level.Warning, cachedMessages);
+        }
+
+        public void WriteError(string message) {
+            consoleInstance.PrintError(message);
+            cachedMessages[0] = message;
+            Log.Message(Log.Level.Error, cachedMessages);
+        }
+    }
+}
