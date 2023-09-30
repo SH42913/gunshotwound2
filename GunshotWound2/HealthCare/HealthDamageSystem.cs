e6a2e58262ec26cae99dbf8b622b3601877fc7cb
SH42913 <Shoot913@gmail.com>
2023-09-30 21:43:41 +0400

    Delay health damage while Player is using phone

--- GunshotWound2/HealthCare/HealthDamageSystem.cs
+++ GunshotWound2/HealthCare/HealthDamageSystem.cs
@@ -1,6 +1,5 @@
 ﻿namespace GunshotWound2.HealthCare {
     using System;
-    using GTA;
     using Peds;
     using Scellecs.Morpeh;
 
@@ -9,7 +8,7 @@
         private Filter peds;
         private Stash<Health> healthStash;
 
-        public Scellecs.Morpeh.World World { get; set; }
+        public World World { get; set; }
 
         public HealthDamageSystem(SharedData sharedData) {
             this.sharedData = sharedData;
@@ -21,17 +20,18 @@
         }
 
         public void OnUpdate(float deltaTime) {
-            foreach (Scellecs.Morpeh.Entity entity in peds) {
+            foreach (Entity entity in peds) {
                 ref Health health = ref healthStash.Get(entity);
-                if (health.damage < 1f) {
+                ref ConvertedPed convertedPed = ref entity.GetComponent<ConvertedPed>();
+                float threshold = convertedPed.isPlayer && convertedPed.IsUsingPhone() ? 5f : 1f;
+                if (health.damage < threshold) {
                     continue;
                 }
 
                 var currentDamage = (int)Math.Floor(health.damage);
                 health.damage -= currentDamage;
-
-                ref ConvertedPed convertedPed = ref entity.GetComponent<ConvertedPed>();
                 convertedPed.thisPed.Health -= currentDamage;
+
 #if DEBUG
                 var healthString = $"Current:{convertedPed.thisPed.Health.ToString()} / Max:{health.max.ToString()}";
                 sharedData.logger.WriteInfo($"Dealing {currentDamage.ToString()} damage to {convertedPed.name}. {healthString}");
