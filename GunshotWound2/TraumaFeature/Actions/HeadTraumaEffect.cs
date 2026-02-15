64d2031a033c65789281bed88b057a2ade223980
SH42913 <shoot913@gmail.com>
2026-02-15 15:35:25 +0400

    Improvements to headshot reaction

--- GunshotWound2/TraumaFeature/Actions/HeadTraumaEffect.cs
+++ GunshotWound2/TraumaFeature/Actions/HeadTraumaEffect.cs
@@ -1,22 +1,29 @@
 namespace GunshotWound2.TraumaFeature {
     using Configs;
+    using GTA;
+    using GTA.NaturalMotion;
     using HealthFeature;
     using PedsFeature;
     using Scellecs.Morpeh;
     using Utils;
+    using EcsEntity = Scellecs.Morpeh.Entity;
 
     public sealed class HeadTraumaEffect : SpineTraumaEffect {
+        private static readonly Message STIFFNESS_MESSAGE = new("setStiffness");
+
         public override string PlayerMessage => null;
         public override string ManMessage => null;
         public override string WomanMessage => null;
 
         public HeadTraumaEffect(SharedData sharedData) : base(sharedData) { }
 
-        public override void Apply(Entity entity, in BodyPartConfig.BodyPart bodyPart, ref ConvertedPed convertedPed) {
+        public override void Apply(EcsEntity entity, in BodyPartConfig.BodyPart bodyPart, ref ConvertedPed convertedPed) {
             base.Apply(entity, bodyPart, ref convertedPed);
-            convertedPed.thisPed.IsPainAudioEnabled = false;
-            convertedPed.thisPed.StopCurrentPlayingAmbientSpeech();
-            convertedPed.thisPed.StopCurrentPlayingSpeech();
+
+            Ped ped = convertedPed.thisPed;
+            ped.IsPainAudioEnabled = false;
+            ped.StopCurrentPlayingAmbientSpeech();
+            ped.StopCurrentPlayingSpeech();
 
             string speech;
             if (convertedPed.isPlayer) {
@@ -26,25 +33,21 @@ namespace GunshotWound2.TraumaFeature {
                 speech = sharedData.random.Next(StatusFeature.Statuses.UnconsciousStatus.NON_PLAYER_DEATH_AMBIENT);
             }
 
-            convertedPed.thisPed.PlayAmbientSpeech(speech);
+            ped.PlayAmbientSpeech(speech);
 
             string facialAnimName = sharedData.random.Next(StatusFeature.Statuses.UnconsciousStatus.DEATH_MOODS);
             convertedPed.RequestFacialIdleAnim(facialAnimName);
             convertedPed.facialIdleAnimLock = true;
-
-            if (convertedPed.thisPed.CurrentVehicle.IsValid()) {
-                GTAHelpers.PlayDeathAnimationInVehicle(convertedPed.thisPed);
-            }
         }
 
-        public override void Repeat(Entity entity, ref ConvertedPed convertedPed) {
+        public override void Repeat(EcsEntity entity, ref ConvertedPed convertedPed) {
             base.Repeat(entity, ref convertedPed);
 
             string reason = sharedData.localeConfig.GetTranslation("Traumas.BleedingInHead");
             entity.GetComponent<Health>().InstantKill(reason);
         }
 
-        public override void Cancel(Entity entity, ref ConvertedPed convertedPed) {
+        public override void Cancel(EcsEntity entity, ref ConvertedPed convertedPed) {
             base.Cancel(entity, ref convertedPed);
             convertedPed.thisPed.IsPainAudioEnabled = true;
             convertedPed.facialIdleAnimLock = false;
@@ -57,6 +60,20 @@ namespace GunshotWound2.TraumaFeature {
             if (convertedPed.thisPed.CurrentVehicle.IsValid()) {
                 convertedPed.thisPed.Task.ClearAll();
             }
+        }
+
+        protected override ConvertedPed.NaturalMotionBuilder GetNMBuilder() {
+            return static (sharedData, _, ped) => {
+                float stiffness = sharedData.random.NextFloat(4f, 12f);
+                STIFFNESS_MESSAGE.SetArgument("bodyStiffness", stiffness);
+                STIFFNESS_MESSAGE.SendTo(ped);
+
+                return new BodyRelaxHelper(ped) {
+                    Relaxation = sharedData.random.NextFloat(0f, 50f),
+                    Damping = sharedData.random.NextFloat(1f, 2f),
+                    HoldPose = true,
+                };
+            };
         }
     }
 }
